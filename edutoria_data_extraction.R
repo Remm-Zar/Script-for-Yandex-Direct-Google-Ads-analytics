ee550284b6b23a02dc20ac8bbdbf341c8fa2e41e
egukasyan <edwardezs1337@gmail.com>
2022-07-07 22:43:57 +0300

    Test 3

--- edutoria_data_extraction.R
+++ edutoria_data_extraction.R
@@ -1,6 +1,11 @@
-install.packages("googleAuthR")
-install.packages("googleAnalyticsR")
+#install.packages("googleAuthR")
+#install.packages("googleAnalyticsR")
+#install.packages("openxlsx")
+#install.packages('Rcpp')
+library(Rcpp)
 library(googleAnalyticsR)
+library(openxlsx)
+
 
 # Authentification
 googleAuthR::gar_auth_service(
@@ -17,11 +22,17 @@ googleAuthR::gar_set_client(
 accounts <- ga_account_list()
 accounts[, c("accountName", "webPropertyName", "viewId", "viewName")]
 
-sessions <- google_analytics(
+# Getting data from GA
+edutoriaDataList <- google_analytics(
   viewId = 268316287,
-  date_range = c("2022-07-01", "2022-07-05"), 
-  metrics = "sessions", 
-  dimensions = "date"
+  date_range = c("2022-07-01","2022-07-07"),
+  metrics = c("adCost","adClicks","pageViews","sessions","bounces","goal2Value","goal3Completions"), 
+  dimensions = c("medium","source","campaign"),
+  filtersExpression = "ga:source==yandex;ga:medium==cpc"
 )
 
-sessions
+#Converting data to Excel file
+edutoriaDataFrame<-as.data.frame(edutoriaDataList)
+headlineStyle <- createStyle(halign = "center", textDecoration = "Bold", fontSize = 12)
+write.xlsx(edutoriaDataFrame,"data.xlsx", headerStyle = headlineStyle, tableStyle = "TableStyleLight1")
+
